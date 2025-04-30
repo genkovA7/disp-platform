4f78328406dd63a45753950ed5a48e17ede71530
Simeon Genkov <genkov@Simeons-iMac.local>
2025-04-30 14:40:10 +0200

    Fix: specify template folder for Flask

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # <-- Това е важно!
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Това е ключовото
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
