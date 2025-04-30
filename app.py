96ae83c9835fecd6ad6a7c8ea13a616b4e407c75
Simeon Genkov <genkov@Simeons-iMac.local>
2025-04-30 14:29:36 +0200

    Начална страница с HTML шаблон

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+from flask import Flask, render_template
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Добре дошъл в D.I.S.P.!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
